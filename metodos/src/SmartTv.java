7a0b8b943cf03e63a8c1cab28e2f88f8e30eeff8
Ryan Souza22 <ryanp.souza@hotmail.com>
2023-02-21 20:34:52 -0300

    Exercicios Metodos Ok

--- metodos/src/SmartTv.java
+++ metodos/src/SmartTv.java
@@ -2,4 +2,28 @@ public class SmartTv {
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void aumentaVolume(){
+        volume++;
+    }
+    public void abaixaVolume(){
+        volume--;
+    }
+
+    public void aumentarCanal(){
+        canal++;
+    }
+    public void diminuirCanal(){
+        canal--;
+    }
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+    }
 }
--- metodos/src/Usuario.java
+++ metodos/src/Usuario.java
@@ -4,6 +4,17 @@ public class Usuario {
 
         System.out.println("Estado da TV: " + tv.ligada);
         System.out.println("Canal: " + tv.canal);
+        System.out.println("Volume: " + tv.volume + "\n");
+
+        tv.ligar();
+        tv.aumentaVolume();
+        tv.aumentaVolume();
+        tv.abaixaVolume();
+        tv.mudarCanal(14);
+
+        System.out.println("Estado da TV: " + tv.ligada);
+        System.out.println("Canal: " + tv.canal);
         System.out.println("Volume: " + tv.volume);
+
     }
 }
