9f4026da24ae1d19d1f7fd659a2ee98e6c2b475b
Ryan Souza <102931113+RyanSouza22@users.noreply.github.com>
2023-02-24 08:30:23 -0300

    Delete Escopo-e-Estruturas-de-Controle-Java directory

--- Escopo-e-Estruturas-de-Controle-Java/desafio-controle-fluxo/src/ParametrosInvalidosException.java
+++
@@ -1,5 +0,0 @@
-public class ParametrosInvalidosException extends Exception{
-   public static void main(String[] args) {
-        System.out.println("O segundo parâmetro deve ser maior que o primeiro");
-   }
-}
